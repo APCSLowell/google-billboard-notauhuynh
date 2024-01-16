b7dd8e008411171b9574756248e26e2ad3fb0847
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-01-16 10:25:59 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,9 +1,10 @@
-  
+ 
+  public class GoogleBillboard {
+     
     public String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
     public int firstPos = 2;
     public int secondPos = 12;
     public int count = 0;
-  public class GoogleBillboard {
 
     public void main(final String[] args) {
       while (count < 1) {
